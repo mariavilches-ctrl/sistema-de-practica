63075ae706c124443d8abf0cc116c5ca1ed8b61b
kakoskz <francoescares5@gmail.com>
2025-11-20 15:49:14 -0300

    Creo tabla Carrera
    Añadiendo la tabla de carrera con sus propios atributos

---
+++ Base De Datos/Tablas.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE Carrera (
+    idCarrera INT IDENTITY(1,1) PRIMARY KEY,
+    nombre VARCHAR(150) NOT NULL,
+    directorCarrera VARCHAR(150) NOT NULL,
+    facultad VARCHAR(200) NOT NULL
+);
+
+
+
